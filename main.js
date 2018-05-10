0a15972b4816be7ac3e3094641f5f71421d205ef
Drechsler <johannes.drechsler@arvatosystems.com>
2018-05-10 13:01:35 -0400

    renaming firestore to db

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { store } from "/store.js";
-import { billsRefSorted } from "/firestore.js";
+import { billsRefSorted } from "/db.js";
 
 document.onselectstart = function () { return false; } // ie
 document.onmousedown = function () { return false; } // others
