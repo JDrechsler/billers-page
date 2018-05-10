e6532ed14bddc3448d973f89bf665866168af11d
Drechsler <johannes.drechsler@arvatosystems.com>
2018-05-10 12:56:24 -0400

    removed sensitive data

--- firestore.js
+++ firestore.js
@@ -1,10 +1,6 @@
 var config = {
-	apiKey: "AIzaSyCJXGDc2kvyJSOSzu_hvhS64sHDnyKn_qE",
-	authDomain: "billersapp.firebaseapp.com",
 	databaseURL: "https://billersapp.firebaseio.com",
 	projectId: "billersapp",
-	storageBucket: "billersapp.appspot.com",
-	messagingSenderId: "5384173140"
 };
 
 firebase.initializeApp(config)
