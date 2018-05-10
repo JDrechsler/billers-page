7e66e3e14abc8ff148fced2133c6b74c41f53eed
Drechsler <johannes.drechsler@arvatosystems.com>
2018-05-10 13:20:58 -0400

    change import style

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ document.onselectstart = function () { return false; } // ie
 document.onmousedown = function () { return false; } // others
 
 Vue.component('home-component', httpVueLoader('components/home-component.vue'));
-Vue.component('card-comp', httpVueLoader("/components/card-component.vue"));
-Vue.component('add-bill-comp', httpVueLoader("/components/add-bill-component.vue"));
-Vue.component('edit-bill-comp', httpVueLoader("/components/edit-bill-component.vue"));
-Vue.component('overview-comp', httpVueLoader("/components/overview-component.vue"));
+Vue.component('card-comp', httpVueLoader("components/card-component.vue"));
+Vue.component('add-bill-comp', httpVueLoader("components/add-bill-component.vue"));
+Vue.component('edit-bill-comp', httpVueLoader("components/edit-bill-component.vue"));
+Vue.component('overview-comp', httpVueLoader("components/overview-component.vue"));
 
 new Vue({
 	el: '#my-app',
